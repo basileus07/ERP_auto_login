cc258a1b95a2405a24f031892df3670e15d2f34e
mynk007 <mynk@users.noreply.github.com>
2021-12-12 17:41:41 +0530

    adding files

---
+++ Erp-login.py
@@ -0,0 +1,210 @@
+from selenium import webdriver
+import login_data as  user
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time 
+
+
+#used to auto install or find path for Chrome Driver in your device
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+
+# path = "C:\Program Files (x86)\chromedriver.exe"
+# driver = webdriver.Chrome(path)
+
+driver.get("https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=D92825882E7CA0FC20EEEF0FBA3D5CC7.worker2&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/menulist.htm")
+
+# driver.maximize_window()  if you want to maximize than remove comment 
+
+
+Email = driver.find_element_by_id("user_id")
+Email.send_keys(user.id)
+
+Password = driver.find_element_by_id("password")
+Password.send_keys(user.pswd)
+
+#waiting  for Security Quesiton to load
+try: 
+    Sec_question = WebDriverWait(driver,20).until(
+        EC.visibility_of_element_located((By.ID,"question"))
+    )
+    for key, value in user.Sec_que.items():
+     if key == Sec_question.text:
+         answer = value
+         break
+     else:
+         continue                       
+except:    
+    driver.quit();
+
+
+ans = driver.find_element_by_id("answer")
+ans.send_keys(answer)
+
+login = driver.find_element_by_id("loginFormSubmitButton")
+print("getting login id")
+
+login.click();
+print("clicked")
+
+#from this line we make script keep running
+temp = input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ login_data.py
@@ -0,0 +1,17 @@
+# login details 
+# Write your id(Roll Numver) in place of ******
+id = "******"     
+
+# Write you passwrod in place of ******
+pswd = "******"    
+
+# Write your all Security question in place of "Question No" and it's answer in place of " Answer"
+Sec_que = dict(
+    {
+        'Question No: 1' : 'Answer',
+        'Question NO: 2' : 'Answer',
+        'Question No: 3' : 'Answer',
+    }
+)
+
+
